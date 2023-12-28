ae5f3f5fa0333c8ab4ab6872ef8e17a6020a876a
Heroxin <hero_xin59@163.com>
2023-12-28 13:40:13 +0800

    项目初始化构建，可正常运行

---
+++ internship-admin/src/main/java/com/heroxin/web/controller/test/HelloController.java
@@ -0,0 +1,12 @@
+package com.heroxin.web.controller.test;
+
+/*
+    @Author Heroxin
+    
+    @Create 2023-12-28-10:51
+
+    @Description:
+*/
+
+public class HelloController {
+}
---
+++ internship-common/src/main/java/com/heroxin/common/utils/PageUtils.java
@@ -0,0 +1,35 @@
+package com.heroxin.common.utils;
+
+import com.github.pagehelper.PageHelper;
+import com.heroxin.common.core.page.TableSupport;
+import com.heroxin.common.utils.sql.SqlUtil;
+import com.heroxin.common.core.page.PageDomain;
+
+/**
+ * 分页工具类
+ * 
+ * @author ruoyi
+ */
+public class PageUtils extends PageHelper
+{
+    /**
+     * 设置请求分页数据
+     */
+    public static void startPage()
+    {
+        PageDomain pageDomain = TableSupport.buildPageRequest();
+        Integer pageNum = pageDomain.getPageNum();
+        Integer pageSize = pageDomain.getPageSize();
+        String orderBy = SqlUtil.escapeOrderBySql(pageDomain.getOrderBy());
+        Boolean reasonable = pageDomain.getReasonable();
+        PageHelper.startPage(pageNum, pageSize, orderBy).setReasonable(reasonable);
+    }
+
+    /**
+     * 清理分页的线程变量
+     */
+    public static void clearPage()
+    {
+        PageHelper.clearPage();
+    }
+}

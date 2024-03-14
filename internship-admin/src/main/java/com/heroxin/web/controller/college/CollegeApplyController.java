fc211e0f69da3cca24fa23bda0a603cb3ee98496
Heroxin <hero_xin59@163.com>
2024-03-14 13:32:09 +0800

    实习申请1.1

---
+++ internship-admin/src/main/java/com/heroxin/web/controller/college/CollegeApplyController.java
@@ -0,0 +1,91 @@
+package com.heroxin.web.controller.college;
+
+import com.heroxin.common.annotation.Log;
+import com.heroxin.common.core.controller.BaseController;
+import com.heroxin.common.core.domain.AjaxResult;
+import com.heroxin.common.core.page.TableDataInfo;
+import com.heroxin.common.enums.BusinessType;
+import com.heroxin.common.utils.poi.ExcelUtil;
+import com.heroxin.internship.domain.InternshipApply;
+import com.heroxin.internship.service.IInternshipApplyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * 实习申请审核信息Controller
+ *
+ * @author heroxin
+ * @date 2024-03-14
+ */
+@RestController
+@RequestMapping("/college/apply")
+public class CollegeApplyController extends BaseController {
+    @Autowired
+    private IInternshipApplyService internshipApplyService;
+
+    /**
+     * 查询实习申请审核信息列表
+     */
+    @PreAuthorize("@ss.hasPermi('college:apply:list')")
+    @GetMapping("/list")
+    public TableDataInfo list(InternshipApply internshipApply) {
+        startPage();
+        List<InternshipApply> list = internshipApplyService.selectInternshipApplyList(internshipApply);
+        return getDataTable(list);
+    }
+
+    /**
+     * 导出实习申请审核信息列表
+     */
+    @PreAuthorize("@ss.hasPermi('college:apply:export')")
+    @Log(title = "实习申请审核信息", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, InternshipApply internshipApply) {
+        List<InternshipApply> list = internshipApplyService.selectInternshipApplyList(internshipApply);
+        ExcelUtil<InternshipApply> util = new ExcelUtil<InternshipApply>(InternshipApply.class);
+        util.exportExcel(response, list, "实习申请审核信息数据");
+    }
+
+    /**
+     * 获取实习申请审核信息详细信息
+     */
+    @PreAuthorize("@ss.hasPermi('college:apply:query')")
+    @GetMapping(value = "/{id}")
+    public AjaxResult getInfo(@PathVariable("id") Long id) {
+        return success(internshipApplyService.selectInternshipApplyById(id));
+    }
+
+    /**
+     * 新增实习申请审核信息
+     */
+    @PreAuthorize("@ss.hasPermi('college:apply:add')")
+    @Log(title = "实习申请审核信息", businessType = BusinessType.INSERT)
+    @PostMapping
+    public AjaxResult add(@RequestBody InternshipApply internshipApply) {
+        return toAjax(internshipApplyService.insertInternshipApply(internshipApply));
+    }
+
+    /**
+     * 修改实习申请审核信息
+     */
+    @PreAuthorize("@ss.hasPermi('college:apply:edit')")
+    @Log(title = "实习申请审核信息", businessType = BusinessType.UPDATE)
+    @PutMapping
+    public AjaxResult edit(@RequestBody InternshipApply internshipApply) {
+        return toAjax(internshipApplyService.updateInternshipApply(internshipApply));
+    }
+
+    /**
+     * 删除实习申请审核信息
+     */
+    @PreAuthorize("@ss.hasPermi('college:apply:remove')")
+    @Log(title = "实习申请审核信息", businessType = BusinessType.DELETE)
+    @DeleteMapping("/{ids}")
+    public AjaxResult remove(@PathVariable Long[] ids) {
+        return toAjax(internshipApplyService.deleteInternshipApplyByIds(ids));
+    }
+}
